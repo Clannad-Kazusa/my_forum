6a76f46312814e53558c104f42bada7e67b9944a
HH <452984071@qq.com>
2018-10-09 23:35:16 +0800

    register.html 重写

--- front/js/main.js
+++ front/js/main.js
@@ -1,104 +1,105 @@
-$(function () {
+$(function(){
 
-    // 打开登录框
-    $('.login_btn').click(function () {
+	// 打开登录框
+	$('.login_btn').click(function(){
         $('.login_form_con').show();
-    })
-
-    // 点击关闭按钮关闭登录框或者注册框
-    $('.shutoff').click(function () {
-        $(this).closest('form').hide();
-    })
+	})
+	
+	// 点击关闭按钮关闭登录框或者注册框
+	$('.shutoff').click(function(){
+		$(this).closest('form').hide();
+	})
 
     // 隐藏错误
-    $(".login_form #mobile").focus(function () {
+    $(".login_form #mobile").focus(function(){
         $("#login-mobile-err").hide();
     });
-    $(".login_form #password").focus(function () {
+    $(".login_form #password").focus(function(){
         $("#login-password-err").hide();
     });
 
-    $(".register_form #mobile").focus(function () {
+    $(".register_form #mobile").focus(function(){
         $("#register-mobile-err").hide();
     });
-    $(".register_form #imagecode").focus(function () {
+    $(".register_form #imagecode").focus(function(){
         $("#register-image-code-err").hide();
     });
-    $(".register_form #smscode").focus(function () {
+    $(".register_form #smscode").focus(function(){
         $("#register-sms-code-err").hide();
     });
-    $(".register_form #password").focus(function () {
+    $(".register_form #password").focus(function(){
         $("#register-password-err").hide();
     });
 
 
-    // 点击输入框，提示文字上移
-    //$('.form_group').on('click focusin',function(){
-    //	$(this).children('.input_tip').animate({'top':-5,'font-size':12},'fast').siblings('input').focus().parent().addClass('hotline');
-    //})
-    $('.form_group').on('click', function () {
+	// 点击输入框，提示文字上移
+	//$('.form_group').on('click focusin',function(){
+	//	$(this).children('.input_tip').animate({'top':-5,'font-size':12},'fast').siblings('input').focus().parent().addClass('hotline');
+	//})
+	$('.form_group').on('click',function(){
         $(this).children('input').focus()
     })
-
-    $('.form_group input').on('focusin', function () {
-        $(this).siblings('.input_tip').animate({'top': -5, 'font-size': 12}, 'fast')
+    
+    $('.form_group input').on('focusin',function(){
+        $(this).siblings('.input_tip').animate({'top':-5,'font-size':12},'fast')
         $(this).parent().addClass('hotline');
     })
 
-    // 输入框失去焦点，如果输入框为空，则提示文字下移
-    $('.form_group input').on('blur focusout', function () {
-        $(this).parent().removeClass('hotline');
-        var val = $(this).val();
-        if (val == '') {
-            $(this).siblings('.input_tip').animate({'top': 22, 'font-size': 14}, 'fast');
-        }
-    })
+	// 输入框失去焦点，如果输入框为空，则提示文字下移
+	$('.form_group input').on('blur focusout',function(){
+		$(this).parent().removeClass('hotline');
+		var val = $(this).val();
+		if(val=='')
+		{
+			$(this).siblings('.input_tip').animate({'top':22,'font-size':14},'fast');
+		}
+	})
 
 
-    // 打开注册框
-    $('.register_btn').click(function () {
-        $('.register_form_con').show();
+	// 打开注册框
+	$('.register_btn').click(function(){
+		$('.register_form_con').show();
+		generateImageCode()
+	})
+
+
+	// 登录框和注册框切换
+	$('.to_register').click(function(){
+		$('.login_form_con').hide();
+		$('.register_form_con').show();
         generateImageCode()
-    })
+	})
 
+	// 登录框和注册框切换
+	$('.to_login').click(function(){
+		$('.login_form_con').show();
+		$('.register_form_con').hide();
+	})
 
-    // 登录框和注册框切换
-    $('.to_register').click(function () {
-        $('.login_form_con').hide();
-        $('.register_form_con').show();
-        generateImageCode()
-    })
+	// 根据地址栏的hash值来显示用户中心对应的菜单
+	var sHash = window.location.hash;
+	if(sHash!=''){
+		var sId = sHash.substring(1);
+		var oNow = $('.'+sId);		
+		var iNowIndex = oNow.index();
+		$('.option_list li').eq(iNowIndex).addClass('active').siblings().removeClass('active');
+		oNow.show().siblings().hide();
+	}
 
-    // 登录框和注册框切换
-    $('.to_login').click(function () {
-        $('.login_form_con').show();
-        $('.register_form_con').hide();
-    })
+	// 用户中心菜单切换
+	var $li = $('.option_list li');
+	var $frame = $('#main_frame');
 
-    // 根据地址栏的hash值来显示用户中心对应的菜单
-    var sHash = window.location.hash;
-    if (sHash != '') {
-        var sId = sHash.substring(1);
-        var oNow = $('.' + sId);
-        var iNowIndex = oNow.index();
-        $('.option_list li').eq(iNowIndex).addClass('active').siblings().removeClass('active');
-        oNow.show().siblings().hide();
-    }
-
-    // 用户中心菜单切换
-    var $li = $('.option_list li');
-    var $frame = $('#main_frame');
-
-    $li.click(function () {
-        if ($(this).index() == 5) {
-            $('#main_frame').css({'height': 900});
-        }
-        else {
-            $('#main_frame').css({'height': 660});
-        }
-        $(this).addClass('active').siblings().removeClass('active');
-        $(this).find('a')[0].click()
-    })
+	$li.click(function(){
+		if($(this).index()==5){
+			$('#main_frame').css({'height':900});
+		}
+		else{
+			$('#main_frame').css({'height':660});
+		}
+		$(this).addClass('active').siblings().removeClass('active');
+		$(this).find('a')[0].click()
+	})
 
     // TODO 登录表单提交
     $(".login_form_con").submit(function (e) {
@@ -118,20 +119,21 @@ $(function () {
 
         // 发起登录请求
         var params = {
-            "mobile":mobile,
-            "password":password,
+        "mobile": mobile,
+        "password": password,
         }
 
         $.ajax({
-            url: "/passport/login",
-            method:"post",
+            url:"/passport/login",
+            method: "post",
             headers:{
                 "X-CSRFToken":getCookie("csrf_token")
             },
             data: JSON.stringify(params),
-            contentType:"application/json",
-            success:function (resp) {
-                if (resp.errno =="0"){
+            contentType: "application/json",
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // 刷新当前界面
                     location.reload();
                 }else {
                     $("#login-password-err").html(resp.errmsg)
@@ -147,12 +149,12 @@ $(function () {
         // 阻止默认提交操作
         e.preventDefault()
 
-        // 取到用户输入的内容
-        var mobile = $("#register_mobile").val()
+		// 取到用户输入的内容
+        var mobile = $("#register_mobile").val();
         var smscode = $("#smscode").val()
         var password = $("#register_password").val()
 
-        if (!mobile) {
+		if (!mobile) {
             $("#register-mobile-err").show();
             return;
         }
@@ -166,7 +168,7 @@ $(function () {
             return;
         }
 
-        if (password.length < 6) {
+		if (password.length < 6) {
             $("#register-password-err").html("密码长度不能少于6位");
             $("#register-password-err").show();
             return;
@@ -174,21 +176,24 @@ $(function () {
 
         // 发起注册请求
         var params = {
-            "mobile": mobile,
-            "smscode": smscode,
-            "password": password,
+        "mobile": mobile,
+        "smscode": smscode,
+        "password": password,
         }
 
         $.ajax({
-            url: "/passport/register",
+            url:"/passport/register",
             type: "post",
+            headers:{
+                "X-CSRFToken":getCookie("csrf_token")
+            },
             data: JSON.stringify(params),
             contentType: "application/json",
             success: function (resp) {
-                if (resp.errno == "0") {
+                if (resp.errno == "0"){
                     // 刷新当前界面
                     location.reload()
-                } else {
+                }else {
                     $("#register-password-err").html(resp.errmsg)
                     $("#register-password-err").show()
                 }
@@ -201,14 +206,14 @@ var imageCodeId = ""
 
 // TODO 生成一个图片验证码的编号，并设置页面中图片验证码img标签的src属性
 function generateImageCode() {
-    // 1. 生成一个编号
+ // 1. 生成一个编号
     // 严格一点的使用uuid保证编号唯一， 不是很严谨的情况下，也可以使用时间戳
     imageCodeId = generateUUID();
 
     // 2. 拼接验证码地址
     var imageCodeUrl = "/passport/image_code?code_id=" + imageCodeId;
     // 3. 设置页面中图片验证码img标签的src属性
-    $(".get_pic_code").attr("src", imageCodeUrl)
+   $(".get_pic_code").attr("src", imageCodeUrl)
 }
 
 // 发送短信验证码
@@ -218,14 +223,16 @@ function sendSMSCode() {
     var mobile = $("#register_mobile").val();
     if (!mobile) {
         $("#register-mobile-err").html("请填写正确的手机号！");
-        $("#register-mobile-err").show();
+        // $("#register-mobile-err").show();
+        $("#register-mobile-err").toggle();
         $(".get_code").attr("onclick", "sendSMSCode();");
         return;
     }
     var imageCode = $("#imagecode").val();
     if (!imageCode) {
-        $("#image-code-err").html("请填写验证码！");
-        $("#image-code-err").show();
+        $("#register-image-code-err").html("请填写验证码！");
+        // $("#register-image-code-err").show();
+        $("#register-image-code-err").toggle();
         $(".get_code").attr("onclick", "sendSMSCode();");
         return;
     }
@@ -236,10 +243,9 @@ function sendSMSCode() {
         "image_code": imageCode,
         "image_code_id": imageCodeId
     }
-
     $.ajax({
         // 请求地址
-        url: "/passport/sms_code",
+        url: "/passport/smscode",
         // 请求方式
         method: "POST",
         // 请求内容
@@ -248,6 +254,9 @@ function sendSMSCode() {
         contentType: "application/json",
         // 响应数据的格式
         dataType: "json",
+        headers:{
+                "X-CSRFToken":getCookie("csrf_token")
+            },
         success: function (resp) {
             if (resp.errno == "0") {
                 // 倒计时60秒，60秒后允许用户再次点击发送短信验证码的按钮
@@ -280,12 +289,11 @@ function sendSMSCode() {
             }
         }
     })
-
 }
 
 // 退出登陆
 function logout() {
-    $.get("/passport/logout", function (resp) {
+    $.get("/passport/logout",function (resp) {
         //刷新当前页面
         location.reload()
     })
@@ -303,9 +311,9 @@ function fnChangeMenu(n) {
 
 // 一般页面的iframe的高度是660
 // 新闻发布页面iframe的高度是900
-function fnSetIframeHeight(num) {
-    var $frame = $('#main_frame');
-    $frame.css({'height': num});
+function fnSetIframeHeight(num){
+	var $frame = $('#main_frame');
+	$frame.css({'height':num});
 }
 
 function getCookie(name) {
@@ -315,13 +323,13 @@ function getCookie(name) {
 
 function generateUUID() {
     var d = new Date().getTime();
-    if (window.performance && typeof window.performance.now === "function") {
+    if(window.performance && typeof window.performance.now === "function"){
         d += performance.now(); //use high-precision timer if available
     }
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = (d + Math.random() * 16) % 16 | 0;
-        d = Math.floor(d / 16);
-        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
     });
     return uuid;
 }
--- front/js/register.js
+++ front/js/register.js
@@ -1,3 +1,6 @@
+/**
+ * Created by python on 18-10-9.
+ */
 var host = "http://127.0.0.1:8000/";
 var sending_flag = false;
 var error_name = false;
@@ -6,54 +9,30 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
-var user_login = function(){
-    const params = {
-        "username": $("#username").val(),
-        "password": $("#password").val()
-    };
-    $.ajax({
-
-        type:"post",
-        url:host +'users/auths/',
-        contentType: "application/json",
-        headers: {
-            "X-CSRFToken": getCookie("csrftoken")
-        },
-        // 跨域
-        xhrFields: {withCredentials: true},
-        data: JSON.stringify(params),
-
-    })
-    .done(function (response) {
-
-            sessionStorage.clear();
-            localStorage.clear();
-            // localStorage.token = response.data.token;
-            if($("#brand1").val()){
-                localStorage.username = response.username;
-                localStorage.user_id = response.user_id;
-            }
-            else {
-                sessionStorage.username = response.username;
-                sessionStorage.user_id = response.user_id;
-            }
-            location.href = '/index.html';
-        })
-    .fail(function (response) {
-    	console.log()
-        alert("用户名或密码错误")
-    })
-};
-
+function ismail(obj){
+      var reg=/^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
+      if(!reg.test(obj.value)){
+		return false;
+      }
+      return true;
+    }
 
 var send_email = function () {
-    // if(sending_flag == true){
-    //     return;
-    // }
-    // if (error_name == true) {
-    //     sending_flag = false;
-    //     return;
-    // }
+	obj = $("#mail input").val();
+	if(ismail(obj) == false){
+		$("#mail .tips").text("请正确填写邮箱！");
+		$("#mail .tips").css("color",'red');
+		return;
+	}
+
+	console.log(obj);
+    if(sending_flag == true){
+        return;
+    }
+    if (error_name == true) {
+        sending_flag = false;
+        return;
+    }
 
     var num = 60;
     var t = setInterval(function(){
@@ -63,20 +42,20 @@ var send_email = function () {
             clearInterval(t);
             sending_flag = false;
             // 将点击获取验证码的按钮展示的文本回复成原始文本
-            // $('#sendsms').text('发送');
+            $('#sendsms').text('发送');
         }
         else {
             sending_flag = true;
             num -= 1;
-            console.log(num+'秒');
+            // console.log(num+'秒');
             // 展示倒计时信息
-            // $('#sendsms').text(num + '秒');
+            $('#sendsms').text(num + '秒');
         }
     },1000, 60);
 };
 
 $(function(){
-	var stuList = getStuList();
+
 	//聚焦失焦input
 	$('input').eq(0).focus(function(){
 		if($(this).val().length==0){
@@ -88,37 +67,35 @@ $(function(){
 		    $(this).parent().next("div").text("输入你的绅士通关密码");
 		}
 	});
-	// $('input').eq(2).focus(function(){
-	// 	if($(this).val().length==0){
-	// 		$(this).parent().next("div").text("请再次输入密码");
-	// 	}
-	// })
-
 	$('input').eq(2).focus(function(){
+		if($(this).val().length==0){
+			$(this).parent().next("div").text("请再次输入密码");
+		}
+	});
+
+	$('input').eq(3).focus(function(){
 		if($(this).val().length==0){
 			$(this).parent().next().next("div").text("看不清？点击图片更换验证码");
 		}else{
 			$(this).parent().next("div").text("");
 		}
-	})
+	});
 	//input各种判断
 	//用户名：
 	$('input').eq(0).blur(function(){
 		if($(this).val().length==0){
 			$(this).parent().next("div").text("");
 			$(this).parent().next("div").css("color",'#ccc');
-		}else{
-			$(this).parent().next("div").text("");
 		}
-		// else if($(this).val().length>0 && $(this).val().length<4){
-		// 	$(this).parent().next("div").text("长度只能在4-20个字符之间");
-		// 	$(this).parent().next("div").css("color",'red');
-		// }
-			// else if($(this).val().length>=4&& !isNaN($(this).val())){
-		// 	$(this).parent().next("div").text("用户名不能为纯数字");
-		// 	$(this).parent().next("div").css("color",'red');
-		// }
-	})
+		else if($(this).val().length>0 && $(this).val().length<4){
+			$(this).parent().next("div").text("长度只能在4-20个字符之间");
+			$(this).parent().next("div").css("color",'red');
+		}
+			else if($(this).val().length>=4&& !isNaN($(this).val())){
+			$(this).parent().next("div").text("用户名不能为纯数字");
+			$(this).parent().next("div").css("color",'red');
+		}
+	});
 	//密码
 	$('input').eq(1).blur(function(){
 		if($(this).val().length==0){
@@ -127,7 +104,7 @@ $(function(){
 		}else{
 			$(this).parent().next("div").text("");
 		}
-	})
+	});
 	// 确认密码
 	$('input').eq(2).blur(function(){
 		if($(this).val().length==0){
@@ -139,7 +116,7 @@ $(function(){
 		}else{
 			$(this).parent().next("div").text("");
 		}
-	})
+	});
 
 // 	验证码
 //	 验证码刷新
@@ -151,7 +128,7 @@ $(function(){
 		}
 		str1=str1.substring(1)
 		$("#code").text(str1);
-	}
+	};
 	code();
 	$("#code").click(code);
 //	验证码验证
@@ -165,18 +142,24 @@ $(function(){
 		}else{
 			$(this).parent().next().next("div").text("");
 		}
-	})
+	});
+//邮箱验证码
+	$("#sendsms").click(function (e) {
+		send_email();
+		return false;
+    });
+
 //	提交按钮
 	$("#submit_btn").click(function(e){
-		for(var j=0 ;j<4;j++){
+		for(var j=0 ;j<5;j++){
 			if($('input').eq(j).val().length==0){
 				$('input').eq(j).focus();
-				// if(j==3){
-				// 	$('input').eq(j).parent().next().next("div").text("此处不能为空");
-				// 	$('input').eq(j).parent().next().next("div").css("color",'red');
-				// 	e.preventDefault();
-				// 	return;
-				// }
+				if(j==4){
+					$('input').eq(j).parent().next().next("div").text("此处不能为空");
+					$('input').eq(j).parent().next().next("div").css("color",'red');
+					e.preventDefault();
+					return;
+				}
 				$('input').eq(j).parent().next(".tips").text("此处不能为空");
 				$('input').eq(j).parent().next(".tips").css("color",'red');
 				e.preventDefault();
@@ -192,7 +175,7 @@ $(function(){
 			//发送用户信息
 			//$("form").submit();
 			e.preventDefault();
-			user_login();
+
 
 		}else{
 			$("#xieyi").next().next().next(".tips").text("请勾选协议");
@@ -200,23 +183,7 @@ $(function(){
 			e.preventDefault();
 			return;
 		}
-	})
+	});
 
-//  建立构造函数，构造学生信息模板
-	function Student(name,password,tel,id){
-         this.name = name;
-         this.password = password;
-         this.tel = tel;
-         this.id = id;
-     }
-//	获取之前所有已经注册的用户集合
-	function getStuList(){
-	    var list = localStorage.getItem('stuList');
-	    if(list != null){
-	        return JSON.parse(list);
-	    }else{
-	        return new Array();
-	    }
-	}
 
 });
